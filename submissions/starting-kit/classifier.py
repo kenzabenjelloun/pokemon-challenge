e5462fbc4787fba5a063a76d89b772c55c3af85e
kenzabenjelloun <74252706+kenzabenjelloun@users.noreply.github.com>
2023-02-21 20:53:45 +0100

    Create classifier.py

---
+++ submissions/starting-kit/classifier.py
@@ -0,0 +1,11 @@
+from sklearn.base import BaseEstimator
+
+class PokemonClassifier(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+       pass
