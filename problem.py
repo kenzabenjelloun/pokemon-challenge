61cfe6a42c9fd42f25bea76a614c099f704308ec
Vincent-tch <80849338+Vincent-tch@users.noreply.github.com>
2023-02-27 18:46:22 +0100

    Update problem.py

--- problem.py
+++ problem.py
@@ -5,6 +5,7 @@ import rampwf as rw
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 from rampwf.score_types.classifier_base import ClassifierBaseScoreType
 from rampwf.score_types.macro_averaged_recall import MacroAveragedRecall
 
@@ -43,10 +44,35 @@ class BalancedAccuracy(ClassifierBaseScoreType):
         score = (tpr - base_tpr) / (1 - base_tpr)
         return score
 
+class ROCAUC(BaseScoreType):
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+
+    def __init__(self, name='roc_auc', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def score_function(self, ground_truths, predictions):
+        """A hybrid score.
+        It tests the predicted _probability_ of the second class
+        against the true _label index_ (which is 0 if the first label is the
+        ground truth, and 1 if it is not, in other words, it is the
+        true probability of the second class). Thus we have to override the
+        `Base` function here
+        """
+        y_proba = predictions.y_pred[:, 1]
+        y_true_proba = ground_truths.y_pred_label_index
+        self.check_y_pred_dimensions(y_true_proba, y_proba)
+        return self.__call__(y_true_proba, y_proba)
+
+    def __call__(self, y_true_proba, y_proba):
+        return roc_auc_score(y_true_proba, y_proba)
 
 score_types = [
     ClassificationError(name='Classification_error', precision=2),
     BalancedAccuracy(name='Balanced_accuracy', precision=2),
+    ROCAUC(name='Roc_Auc', precision=2),
 ]
 
 def get_cv(X, y):
